0eab876700b9027ec434cbf69acd659afeac8ee1
guen <guen5997@naver.com>
2024-01-09 23:57:19 +0900

    init

--- app/src/main/java/com/carrot/carrotdiary/MainActivity.kt
+++ app/src/main/java/com/carrot/carrotdiary/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 좋아 너무 좋아
     }
 }
