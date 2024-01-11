cb33fd720f1cc3d8fe635641bd8d87d43c96c79e
guen <guen5997@naver.com>
2024-01-11 21:01:28 +0900

    refactor: dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,14 +36,11 @@ android {
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    //androidx
+    implementation(KTX.CORE)
+    implementation(AndroidX.APP_COMPAT)
+    implementation (AndroidX.MATERIAL)
+    implementation (AndroidX.CONSTRAINT_LAYOUT)
 
     //nav component
     implementation (NavComponent.NAVIGATION_FRAGMENT)
@@ -51,5 +48,15 @@ dependencies {
     implementation (NavComponent.NAVIGATION_DYNAMIC_FEATURES_FRAGMENT)
     androidTestImplementation (NavComponent.NAVIGATION_TESTING)
     implementation (NavComponent.NAVIGATION_COMPOSE)
+
+    // ViewModel
+    implementation (AndroidX.LIFECYCLE_VIEW_MODEL)
+
+    //by viewModel
+    implementation (AndroidX.ACTIVITY)
+    implementation (AndroidX.FRAGMENT)
+
+    // ViewModel
+    implementation (AndroidX.LIFECYCLE_VIEW_MODEL)
 
 }
--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -11,3 +11,18 @@ object NavComponent {
     const val NAVIGATION_TESTING = "androidx.navigation:navigation-testing:$NAV_VERSION"
     const val NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.4.0-alpha10"
 }
+
+object AndroidX {
+    const val MATERIAL = "androidx.compose.material:material:1.0.0-rc02"
+    const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.4"
+    const val APP_COMPAT = "androidx.appcompat:appcompat:1.6.1"
+    const val LIFECYCLE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0"
+    const val LIFECYCLE_LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.3.1"
+    const val ACTIVITY = "androidx.activity:activity-ktx:1.3.1"
+    const val FRAGMENT = "androidx.fragment:fragment-ktx:1.5.2"
+}
+
+object KTX {
+    const val CORE = "androidx.core:core-ktx:1.6.0"
+}
+
