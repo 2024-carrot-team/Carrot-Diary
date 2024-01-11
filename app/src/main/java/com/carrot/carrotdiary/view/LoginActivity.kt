fa7bc47857b07e9827ad7bc64d399211d7d47118
guen <guen5997@naver.com>
2024-01-11 21:55:12 +0900

    add: 와이어 프레임, btnLogin clickListener

--- app/src/main/java/com/carrot/carrotdiary/view/LoginActivity.kt
+++ app/src/main/java/com/carrot/carrotdiary/view/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.carrot.carrotdiary.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.carrot.carrotdiary.R
+import com.carrot.carrotdiary.databinding.ActivityLoginBinding
+import com.carrot.carrotdiary.view.main.MainActivity
+
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        binding.btnLogin.setOnClickListener {
+            v ->
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
