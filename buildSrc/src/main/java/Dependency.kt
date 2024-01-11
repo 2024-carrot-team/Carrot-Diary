c7dc793ca15ee84d836f6ec05b26d18195115c0a
guen <guen5997@naver.com>
2024-01-11 20:13:06 +0900

    add: Version file

--- buildSrc/src/main/java/Dependency.kt
+++ buildSrc/src/main/java/Dependency.kt
@@ -1,3 +1,13 @@
-object Versions {
+import Versions.NAV_VERSION
 
+object Versions {
+    const val NAV_VERSION = "2.4.0-alpha10"
+}
+
+object NavComponent {
+    const val NAVIGATION_FRAGMENT = "androidx.navigation:navigation-fragment-ktx:$NAV_VERSION"
+    const val NAVIGATION_UI = "androidx.navigation:navigation-ui-ktx:$NAV_VERSION"
+    const val NAVIGATION_DYNAMIC_FEATURES_FRAGMENT = "androidx.navigation:navigation-dynamic-features-fragment:$NAV_VERSION"
+    const val NAVIGATION_TESTING = "androidx.navigation:navigation-testing:$NAV_VERSION"
+    const val NAVIGATION_COMPOSE = "androidx.navigation:navigation-compose:2.4.0-alpha10"
 }
