3e6c18baeaa06547d677829cd2bd5aba4d56986c
guen <guen5997@naver.com>
2024-01-12 22:44:26 +0900

    refactor: setting clickListener

--- app/src/main/java/com/carrot/carrotdiary/view/main/DiaryFragment.kt
+++ app/src/main/java/com/carrot/carrotdiary/view/main/DiaryFragment.kt
@@ -1,14 +1,17 @@
 package com.carrot.carrotdiary.view.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.carrot.carrotdiary.R
+import com.carrot.carrotdiary.databinding.FragmentDiaryBinding
+import com.carrot.carrotdiary.view.SettingActivity
 
 class DiaryFragment : Fragment() {
 
+    private lateinit var binding: FragmentDiaryBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -17,7 +20,14 @@ class DiaryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+
+        binding = FragmentDiaryBinding.inflate(inflater, container, false)
+
+        binding.buttonSettingMain.setOnClickListener {
+            val intent = Intent(activity, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
+        return binding.root
     }
 }
