ab01c0200769dda0a02fab0cc954d32bfaa5d214
guen <guen5997@naver.com>
2024-01-14 22:42:29 +0900

    add: showDailyAdapter, ShowDailyFragment

---
+++ app/src/main/java/com/carrot/carrotdiary/view/main/ShowDailyAdapter.kt
@@ -0,0 +1,62 @@
+package com.carrot.carrotdiary.view.main
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.carrot.carrotdiary.databinding.ItemDailyBinding
+import com.carrot.carrotdiary.model.Daily
+
+
+// 나중에 페이징 어뎁터로 리펙토링
+class ShowDailyAdapter(private val context: Context) :
+    ListAdapter<Daily, ShowDailyAdapter.DailyViewHolder>(DIFF_CALLBACK) {
+    companion object {
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Daily>() {
+            override fun areItemsTheSame(oldItem: Daily, newItem: Daily): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: Daily,
+                newItem: Daily
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+        }
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DailyViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return DailyViewHolder(
+            ItemDailyBinding.inflate(
+                inflater,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: DailyViewHolder, position: Int) {
+        val item = getItem(position)
+        if (item != null) {
+            holder.bind(item, context)
+        }
+    }
+
+    class DailyViewHolder(private val itemDailyBinding: ItemDailyBinding) :
+        RecyclerView.ViewHolder(itemDailyBinding.root) {
+        fun bind(item: Daily, context: Context) {
+            itemDailyBinding.text.text = item.id.toString()
+            itemDailyBinding.itemDaily.setOnClickListener {
+//                val intent = Intent(context, ::class.java)
+//                startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/carrot/carrotdiary/view/main/ShowDailyFragment.kt
@@ -0,0 +1,54 @@
+package com.carrot.carrotdiary.view.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.carrot.carrotdiary.databinding.FragmentShowDailyBinding
+import com.carrot.carrotdiary.model.Daily
+
+
+class ShowDailyFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding = FragmentShowDailyBinding.inflate(inflater, container, false)
+
+
+        val list = mutableListOf<Daily>()
+
+        for (i in 0 until 10) {
+            val item = Daily(i, null, i, null, null)
+            list.add(i, item)
+        }
+        var adapter = ShowDailyAdapter()
+        binding.recyclerShowShowDaily.adapter = adapter
+        
+        arguments?.takeIf { it.containsKey(DailyAdapter.DAILY_TYPE) }?.apply {
+            var dailryType = getInt(DailyAdapter.DAILY_TYPE)
+            when (dailryType) {
+                0 -> { // 추천 일기 데이터
+                    adapter.submitList(list)
+
+                }
+
+                1 -> { // 팔로우 일기 데이터
+                    adapter.submitList(list)
+                }
+            }
+
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    }
+}
