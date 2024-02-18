c1b9ac22eb9c71d275efe90b7f5ce00d7f30fcd1
guen <guen5997@naver.com>
2024-02-18 22:39:39 +0900

    fix: dailyList fragment -> Activity

--- app/src/main/java/com/carrot/carrotdiary/view/main/dailylist/DailyListActivity.kt
+++ app/src/main/java/com/carrot/carrotdiary/view/main/dailylist/DailyListActivity.kt
@@ -1,13 +1,9 @@
 package com.carrot.carrotdiary.view.main.dailylist
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.carrot.carrotdiary.databinding.FragmentDailyListBinding
+import com.carrot.carrotdiary.databinding.ActivityDailyListBinding
 import com.carrot.carrotdiary.model.Accident
 import com.carrot.carrotdiary.model.Daily
 import com.carrot.carrotdiary.model.Diary
@@ -15,22 +11,14 @@ import com.carrot.carrotdiary.model.User
 import java.text.SimpleDateFormat
 import java.util.Date
 
-class DailyListFragment : Fragment() {
-    private lateinit var binding: FragmentDailyListBinding
-    private lateinit var context: Context
+class DailyListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDailyListBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentDailyListBinding.inflate(inflater, container, false)
-
-        if (container != null) context = container.context
+        binding = ActivityDailyListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Dummy user data
         val dateFormat = SimpleDateFormat("yyyy-MM-dd")
@@ -44,9 +32,15 @@ class DailyListFragment : Fragment() {
         val accident2 = Accident(2, "Slipped on ice", listOf("image3.jpg"), "2024-02-19")
 
         // Dummy dailies
-        val daily1 = Daily(1, Date(), 10, listOf(accident1))
-        val daily2 = Daily(2, Date(), 15, listOf(accident2))
-        val daily3 = Daily(3, Date(), 1, listOf(accident1, accident2))
+        val date1 = Date()
+        val date2 =
+            Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24) // Adding 1 day to current date
+        val date3 =
+            Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 2) // Adding 2 days to current date
+
+        val daily1 = Daily(1, date1, 10, listOf(accident1))
+        val daily2 = Daily(2, date2, 15, listOf(accident2))
+        val daily3 = Daily(3, date3, 1, listOf(accident1, accident2))
 
         // Dummy diary
         val diaryDate = dateFormat.format(Date())
@@ -54,15 +48,12 @@ class DailyListFragment : Fragment() {
             Diary(1, "My Daily Diary", user, listOf(daily1, daily2, daily3), "cover.jpg", diaryDate)
 
 
-        val adapter = DailyListAdapter(context)
+        val adapter = DailyListAdapter(this)
 
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager =
-            LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
         adapter.updateDataset(diary)
-
-        return binding.root
     }
-
 }
