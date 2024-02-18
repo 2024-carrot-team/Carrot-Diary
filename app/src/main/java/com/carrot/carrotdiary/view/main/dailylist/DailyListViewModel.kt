bb0d74ef3a2fd179b0f5d3b0202618c38e2818c6
guen <guen5997@naver.com>
2024-02-18 22:41:49 +0900

    add: viewModel

---
+++ app/src/main/java/com/carrot/carrotdiary/view/main/dailylist/DailyListViewModel.kt
@@ -0,0 +1,6 @@
+package com.carrot.carrotdiary.view.main.dailylist
+
+import androidx.lifecycle.ViewModel
+
+class DailyListViewModel : ViewModel() {
+}
---
+++ app/src/main/java/com/carrot/carrotdiary/view/main/diary/DiaryDetailViewModel.kt
@@ -0,0 +1,6 @@
+package com.carrot.carrotdiary.view.main.diary
+
+import androidx.lifecycle.ViewModel
+
+class DiaryDetailViewModel : ViewModel() {
+}
