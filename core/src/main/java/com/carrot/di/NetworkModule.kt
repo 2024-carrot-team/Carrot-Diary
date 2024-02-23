1e0210251b97144f75c0913d231ffaaec7bca37c
guen <guen5997@naver.com>
2024-02-24 00:01:04 +0900

    add: NetworkModule

---
+++ core/src/main/java/com/carrot/di/NetworkModule.kt
@@ -0,0 +1,66 @@
+package com.carrot.di
+
+import com.carrot.data.remote.api.LoginApiService
+import com.carrot.utils.Utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+object NetworkModule {
+    @Module
+    @InstallIn(SingletonComponent::class)
+    object NetworkModule {
+
+        @Provides
+        @Singleton
+        //okHttp 의존성 주입 (아래 retrofit 의존성 주입에 사용)
+        fun provideHttpClient(): OkHttpClient {
+            return OkHttpClient.Builder()
+                .readTimeout(10, TimeUnit.SECONDS)
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(15, TimeUnit.SECONDS)
+                .addInterceptor(getLoggingInterceptor())
+                .build()
+        }
+
+        @Provides
+        @Singleton
+        //gson 의존성 주입 (아래 retrofit 의존성 주입에 사용)
+        fun provideConverterFactory(): GsonConverterFactory {
+            return GsonConverterFactory.create()
+        }
+
+        @Singleton
+        @Provides
+        //retrofit 의존성 주입 (아래 LoveCalculatorApi 의존성 주입에 사용)
+        fun provideRetrofitInstance(
+            okHttpClient: OkHttpClient,
+            gsonConverterFactory: GsonConverterFactory
+        ): Retrofit {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(okHttpClient)
+                .addConverterFactory(gsonConverterFactory)
+                .build()
+        }
+
+
+        private fun getLoggingInterceptor(): HttpLoggingInterceptor =
+            HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY }
+
+        @Provides
+        @Singleton
+        //Api interface 의존성 주입
+        fun provideApiService(retrofit: Retrofit): LoginApiService {
+            return retrofit.create(LoginApiService::class.java)
+        }
+
+    }
+}
